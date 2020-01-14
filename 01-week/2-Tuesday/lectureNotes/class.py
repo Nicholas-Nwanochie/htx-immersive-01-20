ae94e29c191b54bb2e88da4e4fc0e0cf28b3e745
Veronica <veronica@digitalcrafts.com>
2020-01-14 12:17:05 -0600

    python 101 class notes

---
+++ 01-week/2-Tuesday/lectureNotes/class.py
@@ -0,0 +1,83 @@
+# print("""I'm a string
+#       slkjsl;kdjf
+#       slkdfjslkd
+#       sldkjfsl
+#       ldkfjsldk
+#       lsdkjfs
+#       sldkfjs
+#       lskdjfs
+#       lskdjf
+      
+#       """)
+
+# print("I'm a string")
+# print("slkjsl;kdjf")
+
+
+# lskdjlsk
+
+
+# concatenting
+
+# print("hello " + "world")
+
+# print("""i\'m  a\t\t\t\tstring
+#       lskdjskldjf
+#       slkdjfsldk
+#       \v\v\v
+#       sldkfjsl
+      
+#       """)
+
+# print("hello \nworld")
+
+# a = 20 
+# b = 15
+# c = 3
+
+# print(a + b +c )
+
+name = "foorkan"
+lname = "john"
+
+# output = "good morning {student1} and {student2}".format(student1=name, student2=lname)
+
+# print(output)
+
+# output = "good morning {0} and {0} {0} {1} {1}".format(name, lname)
+
+# print(output)
+
+student1 = 34
+student2 = "diego"
+output = f'hello world {student1} {student2}'
+
+# print(output)
+
+# print(type(student1))
+
+# test = "5" 
+
+# test1 = "5" + 5
+# print(isinstance(test, int))
+
+
+# consolelog = input("Enter your name >>")
+
+# output = f'Hello {consolelog}'
+# dt = type(consolelog)
+
+# print(dt)
+
+input_number_from_user = input("Insert a number >> ")
+
+print(type(input_number_from_user))
+
+
+casted_output = int(input_number_from_user)
+
+print(type(casted_output))
+
+some_math = casted_output * 7
+
+print(some_math)
