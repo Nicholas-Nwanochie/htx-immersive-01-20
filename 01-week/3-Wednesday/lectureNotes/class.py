c9e75d41e7be3897e4725c88c905871036c49652
Veronica <veronica@digitalcrafts.com>
2020-01-15 19:52:03 -0600

    class notes

---
+++ 01-week/3-Wednesday/lectureNotes/class.py
@@ -0,0 +1,193 @@
+
+# day1 = "Sunday"
+# day2 = "Monday"
+# day3 = "Tuesday"
+# day4 = "Wednesday"
+# day5 = "Thursday"
+# day6 = "Friday"
+# day7 = "Saturday"
+
+# day = 
+# num2 = [1, 2]
+
+# number = [num2, 2, 3, 4, 5]
+
+# num_elements = len(number)
+
+# print(num_elements)
+# # print(number[0])
+
+# number[0] = "Monday"
+
+# print(number)
+
+
+# number.append("4")
+
+# print(number)
+
+
+# todos = ["pet the cat", "go to work", "shop for groceries", 
+# "go home", "feed the cat"]
+
+
+# todos.append("binge watch a show")
+# todos.append("go to sleep")
+
+# print(todos)
+
+# index = 0
+# allTodos = ""
+# while index < len(todos):
+#     print(f'{index + 1} : {todos[index]}' )
+#     allTodos += str(index + 1) + ". " + todos[index] + " "
+#     index += 1 
+# print(allTodos)
+
+# a = [1, 4, 7, 9, 10]
+
+# b = [4, 6, 8, 9]
+
+# print(a)
+# print(b)
+
+# print(a + b)
+
+# todos = ["pet the cat", "go to work", "shop for groceries", 
+# "go home", "feed the cat"]
+
+# todos.extend(["binge watch a show", "go to sleep"])
+
+# todos.append([1, 2])
+# print(todos)
+
+# list_a = [1, 4, 7, 9]
+
+# list_b = [5, 7, 9]
+
+# print(list_a + list_b )
+
+# print(list_a)
+
+# todos = ["pet the cat", "go to work", "shop for groceries", 
+# "go home", "feed the cat"]
+
+# print(todos)
+
+# del todos[1]
+
+# print(todos)
+
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+
+# new_nums = numbers[3:]
+
+# print(new_nums)
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.insert(3, 6)
+
+# print(numbers)
+
+
+# poppedValue = numbers.pop()
+
+# print(poppedValue)
+# print(numbers)
+
+# numbers = [1, 2, 3, 4, 5]
+
+
+# # while (len(numbers) > 0):
+# #     print(numbers.pop())
+# #     print(numbers)
+
+# # print('finished')
+
+# result = numbers.index(3)
+
+# print(result)
+
+# students = ["Alina", "Diego", "Kazim", "Austin", "Matt", "Richard"]
+
+
+# result = students.index("Kazim")
+
+# print(result)
+
+# myList = [1, 2, 3, 4, 5]
+
+# newList = myList.copy()
+
+# newList[0] = "changed"
+
+# print(myList)
+# print(newList)
+
+# myNum = 3
+
+# myCopyNum = myNum
+
+# myCopyNum = 4
+
+# print(myNum)
+# print(myCopyNum)
+
+# a = [1,2]
+
+# print(a*3)
+
+# mixed_list = ["hello", "world", "a "]
+# mixed_list.sort()
+
+# print(mixed_list)
+
+
+# my_string = "Hello World"
+
+# print(my_string[6:])
+
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+# index = 0
+
+# while index < len(alphabet):
+#     print(alphabet[index])
+#     index += 1
+
+# print(type(alphabet))
+
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+# alph_list = list(alphabet)
+# print(type(alph_list))
+
+# alph_str = ".".join(alph_list)
+
+# print(alph_str)
+
+# myTuple = (1, 4, 6, "word")
+
+# myTuple[1] = 34
+# print(myTuple[1])
+
+# num = (1, 2, 3)
+# one, two, three = num
+
+# printone)
+
+# num = range(10)
+# print(num)
+
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+# for character in alphabet :
+#     print(character)
+
+
+for index in range(10):
+    print(index)
+    
+Create a loop that prints out every other numbers
+
+1, 3, 5, 7, .....1000
