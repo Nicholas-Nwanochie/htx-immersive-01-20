df52d74cc439e535fd085ccf6900f53fd22c61c0
Veronica <veronica@digitalcrafts.com>
2020-01-30 15:22:51 -0600

    flex box

---
+++ 03-week/1-Monday/lectureNotes/modules.py
@@ -0,0 +1,31 @@
+
+import "../myFolder/random"
+
+places = [1, 2, 3, 4, 5]
+
+
+def pick():
+    return random.choice(places)
+
+
+# print(pick())
+# print(pick())
+# print(pick())
+
+
+# import fast
+
+# place = fast.pick()
+# print("Let's go to", place)
+
+
+# from random import randint
+
+
+# if randint(1, 5) == 2:
+#     print("2 is the number")
+# else:
+#     print("some other number was found")
+    
+
+
