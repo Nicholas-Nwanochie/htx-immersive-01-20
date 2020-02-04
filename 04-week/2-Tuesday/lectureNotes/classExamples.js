6810b6d8cfeac2a42102a7e82f7cb91c51f66e05
Veronica <veronica@digitalcrafts.com>
2020-02-04 12:28:50 -0600

    js 101 lecture notes

---
+++ 04-week/2-Tuesday/lectureNotes/classExamples.js
@@ -0,0 +1,125 @@
+
+// console.log('hello class')
+
+// function myFunction(){
+
+// }
+
+// var school = "dig crafts"
+
+// console.log('hello world')
+
+// "hello"
+// 'hello'
+
+// `sldkjfs
+// sldkfjs
+// lskdjfs
+// `
+
+// "hello" + " " + "there"
+
+// var myString = "hello";
+
+// var myInt = 2.1;
+
+// var myBool = true;
+
+// myBool = true;
+
+// var myString = "hello";
+
+// console.log(myString.length)
+
+
+var num = 1; //integer
+
+var num2 = 1.2;  //float
+
+var myString = "school";
+// console.log(myString.length)
+
+// var concat = "hello" + "world world hello world";
+
+// console.log(concat);
+// console.log(concat.indexOf("world"));
+
+// var boolean = !true; //false
+// console.log(boolean)
+
+// var not = !boolean
+
+// console.log(not);
+
+// var and = boolean && false;
+
+// console.log(and)
+
+// var or = boolean || true;
+
+// console.log(or)
+
+// var equals = true == false; //false
+
+// console.log(equals)
+
+// {/* <=  >=  == */}
+
+
+// var a = 4;
+// var b = 4;
+
+// if (a == b){
+//     console.log("equality")
+// }
+
+
+// var age = 20;
+
+// if (age >= 21) {
+//     console.log("you get free beer");
+// } else if (age < 18) {
+//     console.log("What are you even doing here?");
+// } else {
+//     console.log("Sorry, no beer for you");
+// }
+
+// var expr = "Papayas";
+
+// if(expr == 'Oranges'){
+//     console.log('Oranges are %0.59 a pound')
+// }
+// else if(expr == 'Mangoes'){
+//     console.log("Mangoes")
+
+// }
+// else if(expr == 'Papayas'){
+//     console.log("Mangoes and papayas are $2.79 a pound")
+// }
+// else {
+//     console.log('Sorry, we ar out of' + expr + ".")
+// }
+
+
+// expr = "Papayas";
+
+// switch(expr){
+
+//     case 'Oranges':
+//         console.log('Oranges are $0.59 a pound')
+//         break;
+//     case 'Mangos':
+//         console.log("Mangoes")
+//         break;
+//     case 'Papayas':
+//         console.log("Mangoes and papayas are $2.79 a pound")
+//         break;
+//     default:
+//         console.log('Sorry, we ar out of' + expr + ".")
+//         break;
+
+// }
+
+
+
+
---
+++ 04-week/2-Tuesday/lectureNotes/samplePy.py
@@ -0,0 +1,51 @@
+
+
+# school = "Dig Crafts"
+
+
+# def myFunction():
+#     print('hello world')
+
+# print('hello world')
+
+# myString = "lorem ipsum"
+
+# myString = """
+# lskjsldkfj
+# sldkjfs
+# slkdfs
+# """
+
+
+# "hello" + " " + "there"
+
+myString = "hello"
+
+l = length(myString)
+
+# my_bool = True
+
+# and
+
+# or
+
+# equals
+# ==
+
+# <=
+
+# a = 4
+# b = 3
+
+# if a equals b:
+#     print('equality')
+
+age = 20
+
+if age >= 21:
+    print('you get free beer')
+elif age < 18:
+    print('why are you here')
+    
+
+
