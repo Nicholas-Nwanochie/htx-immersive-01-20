5dc07cd3dc29b21e3eed46f9e3fabf4cf4b61cb1
Veronica <veronica@digitalcrafts.com>
2020-03-16 11:25:24 -0500

    add error to registration page

--- 10-week/1-Monday/lectureNotes/auth/app.js
+++ 10-week/1-Monday/lectureNotes/auth/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
+let db = require('./models');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -22,16 +23,46 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
 
-  res.send('post login')
+  let username = req.body.username;
+  let password = req.body.password;
+
+  db.users.findAll({ where: { username: username } })
+    .then(results => {
+      //[{usrname: value, email: value, password},{}, {}]
+
+      //authenticated
+      if (results.length > 0) {
+        //user has been found,
+
+        //test the pasword
+        //forwarded to protected page
+        res.redirect('/index');
+      }
+      else {
+        res.redirect('/registration')
+      }
+    })
+
+  // res.send('post login')
 })
 
 app.get('/registration', (req, res) => {
 
+  let error = req.query.error;
+  let err = "hidden";
+
+  if (error) {
+    err = "visible"
+  }
   //encrypt the password
   //add information to database 
 
-  res.render('registration')
+  res.render('registration', {
+    error: err
+  })
 })
+
+
 
 
 app.post('/registration', (req, res) => {
@@ -40,14 +71,45 @@ app.post('/registration', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
 
-  // console.log(req.body);
+
+  // test to make sure no duplicate username
+
+  // db.users.findAll({where: {username: username}})
+  // then(results =>{
+  //   if (results.length == 0){
+  //     //no other user in database
+  //   }
+  //   else {
+  //     //duplicates redicred
+  //   }
+  // })
+  // // console.log(req.body);
 
   let passwordEncrypted = bcrypt.hashSync(password, 8);
 
-  console.log(passwordEncrypted);
+  db.users.create({
+    username: username,
+    email: email,
+    password: passwordEncrypted
+  })
+    .then((user) => {
 
-  res.send('post registration')
+      // res.send('post registration');
+      res.redirect('/login')
+    })
+    .catch(error => {
+      // res.send('error duplicate entry');
+      res.redirect('/registration?error=visible')
+    })
+  // console.log(passwordEncrypted);
+
 })
+
+app.get('/error', (req, res) => {
+
+  res.send('error duplicate entry')
+})
+
 
 app.listen(3000, () => {
   console.log('Listening on 3000');
