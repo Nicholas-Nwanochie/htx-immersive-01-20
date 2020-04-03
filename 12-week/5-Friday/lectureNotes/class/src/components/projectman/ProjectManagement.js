8402b3faef2c1e64753432822a59089e662c6f5b
Veronica <veronica@digitalcrafts.com>
2020-04-03 14:14:56 -0500

    project management

--- 12-week/5-Friday/lectureNotes/class/src/components/projectman/ProjectManagement.js
+++ 12-week/5-Friday/lectureNotes/class/src/components/projectman/ProjectManagement.js
@@ -1,11 +1,64 @@
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import AddProject from './AddProject'
 
 class ProjectManagement extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            projects: []
+        }
+    }
+
+    componentWillMount() {
+
+        this.setState({
+            projects: [
+                {
+                    id: uuidv4(),
+                    title: "Javascript",
+                    category: 'Front-End'
+                },
+                {
+                    id: uuidv4(),
+                    title: "Node",
+                    category: 'Back-End'
+                },
+                {
+                    id: uuidv4(),
+                    title: "React",
+                    category: 'Front-End'
+                }
+
+            ]
+        })
+
+    }
+
+    handleAddProject = (newProject) => {
+
+        let oldProjects = this.state.projects;
+
+        oldProjects.push(newProject);
+
+        this.setState({
+
+            projects: oldProjects
+        }, () => {
+            console.log(this.state.projects);
+        })
+    }
+
+
+
     render() {
+
         return (
             <>
-                Project Managent
+                <AddProject addProject={(project) => { this.handleAddProject(project) }} />
             </>
         )
     }
