0fd20f35daf0fc0f159bc9cd61a9e46fb137df40
Veronica <veronica@digitalcrafts.com>
2020-04-07 14:16:13 -0500

    shopping cart redux

---
+++ 13-week/2-Tuesday/lectureNotes/shoppingcart/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Cart from './components/Cart';
+import AddProduct from './components/AddProduct';
+import BaseLayout from './components/layout/BaseLayout'
+
+ReactDOM.render(
+
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path="/" component={Cart} />
+        <Route exact path="/addProduct" component={AddProduct} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>
+
+  ,
+  document.getElementById('root')
+);
