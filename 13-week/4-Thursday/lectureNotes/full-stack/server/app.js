0fc297d4f98f4cadb7f51d9269c0b7781dbccaa3
Veronica <veronica@digitalcrafts.com>
2020-04-09 15:46:17 -0500

    combiing none and react, combineReducers, advanced reducers

---
+++ 13-week/4-Thursday/lectureNotes/full-stack/server/app.js
@@ -0,0 +1,24 @@
+
+let express = require('express');
+let app = express();
+
+
+app.get('/api', (req, res) => {
+
+    res.json([
+        {
+            id: 1,
+            username: 'Daniel'
+        },
+        {
+            id: 2,
+            username: 'Matt'
+        }
+    ])
+
+})
+
+
+app.listen('3001', () => {
+    console.log('listening on port 3001');
+})
